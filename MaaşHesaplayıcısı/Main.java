213960c5d2cbb99436901b1022048900ac1c4676
mehmetcancam <111275137+mehmetcancam@users.noreply.github.com>
2022-08-24 21:43:00 +0300

    Add files via upload

---
+++ MaaşHesaplayıcısı/Main.java
@@ -0,0 +1,10 @@
+package MaaşHesaplayıcısı;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Ali Samed",900,35,2015);
+        e1.print();
+
+    }
+}
