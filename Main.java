7c22345656e281339a6e2f0729784db41022a3d7
mehmetcancam <111275137+mehmetcancam@users.noreply.github.com>
2022-10-02 22:25:04 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package MaaşHesaplayıcısı;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Ali Samed",4500,35,2000);
+        e1.print();
+
+    }
+}
